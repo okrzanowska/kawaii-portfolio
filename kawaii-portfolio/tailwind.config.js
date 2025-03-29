8be32c7c6ad7e690c1e7ecc90a6b350a141db801
Oliwia <oliwia.krzanowska.ok@gmail.com>
2025-03-29 02:31:09 +0000

    Add custom colors and font

---
+++ kawaii-portfolio/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    darkMode: 'class',
+    content: ['./src/**/*.{html,ts}'],
+    theme: {
+        extend: {
+            colors: {
+            kawaiiPink: '#ffb6d9',
+            kawaiiPurple: '#d8b4fe',
+            kawaiiBlue: '#bae6fd',
+            kawaiiMint: '#bbf7d0',
+            darkNight: '#2a2139',
+            },
+            fontFamily: {
+            kawaii: ['"Comic Neue"', 'cursive'],
+            }
+        }
+    },
+    plugins: [],
+}
