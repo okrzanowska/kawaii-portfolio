7c139a89b09be17f1f097aa7754ffb694abb4386
Oliwia <oliwia.krzanowska.ok@gmail.com>
2025-03-29 01:56:11 +0000

    Add first components ans routes

--- kawaii-portfolio/src/app/app.routes.ts
+++ kawaii-portfolio/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'skills', component: SkillsComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'blog', component: BlogComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', redirectTo: '' }
+];
