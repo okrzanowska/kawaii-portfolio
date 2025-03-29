13db57fb5d22632a7029f87a913278cfe666c1e1
Oliwia <oliwia.krzanowska.ok@gmail.com>
2025-03-29 02:32:03 +0000

    Add navbar and footer

--- kawaii-portfolio/src/app/app.component.ts
+++ kawaii-portfolio/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
