fd68a19e81a005cf77b47d23ca9de469218a9574
Oliwia <oliwia.krzanowska.ok@gmail.com>
2025-04-01 01:39:27 +0000

    Add responsiveness

--- kawaii-portfolio/src/app/components/navbar/navbar.component.ts
+++ kawaii-portfolio/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,26 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [ RouterLink ],
+  imports: [ RouterLink, NgIf, NgFor ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  isMenuOpen: boolean = false;
 
+  navLinks = [
+    { path: '/', label: 'Home' },
+    { path: '/about', label: 'About' },
+    { path: '/skills', label: 'Skills' },
+    { path: '/projects', label: 'Projects' },
+    { path: '/blog', label: 'Blog' },
+    { path: '/contact', label: 'Contact' },
+  ];
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
